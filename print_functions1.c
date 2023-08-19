8f3a1248fff5993329f04e680a585a3964c3757c
kwamepaadebra <kwamepolley215@gmail.com>
2023-08-19 09:29:15 -0700

    adding all files

--- print_functions1.c
+++ print_functions1.c
@@ -121,13 +121,13 @@ int print_hexa_upper(va_list types, char buffer[],
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
  * @flag_ch: Calculates active flags
-OBOBOB * @width: get width
+ * @width: get width
  * @precision: Precision specification
-OBOBOB * @size: Size specifier
-OBOBOB * @size: Size specification
-OBOBOB * Return: Number of chars printed
+ * @size: Size specifier
+ * @size: Size specification
+ * Return: Number of chars printed
  */
-OBOBOBint print_hexa(va_list types, char map_to[], char buffer[],
+int print_hexa(va_list types, char map_to[], char buffer[],
 	int flags, char flag_ch, int width, int precision, int size)
 {
 	int i = BUFF_SIZE - 2;
