078aba16fc843aab953c1c31a9ad479891d48578
kwamepaadebra <kwamepolley215@gmail.com>
2023-08-22 15:43:35 -0700

    updating flags.c

--- flags.c
+++ flags.c
@@ -1,34 +1,47 @@
 #include "main.h"
 
 /**
- * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
- * Return: Flags:
+ * get_flags - Parses the format string and calculates active flags.
+ * @format: Formatted string containing the argument formatting flags.
+ * @i: Pointer to the current index in the format string.
+ *
+ * This function processes the format string to identify and calculate
+ * the active flags that affect argument formatting. The active flags
+ * are then represented as a combination of bitmask values.
+ *
+ * Return: Flags: A combination of bitmask values representing the active flags.
  */
 int get_flags(const char *format, int *i)
 {
-	/* - + 0 # ' ' */
-	/* 1 2 4 8  16 */
-	int j, curr_i;
-	int flags = 0;
-	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
-	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
+    /* Flag characters and their corresponding bitmask values */
+    const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+    const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
-	{
-		for (j = 0; FLAGS_CH[j] != '\0'; j++)
-			if (format[curr_i] == FLAGS_CH[j])
-			{
-				flags |= FLAGS_ARR[j];
-				break;
-			}
+    int j, curr_i;
+    int flags = 0;
 
-		if (FLAGS_CH[j] == 0)
-			break;
-	}
+    for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+    {
+        /* Check each character in FLAGS_CH */
+        for (j = 0; FLAGS_CH[j] != '\0'; j++)
+        {
+            if (format[curr_i] == FLAGS_CH[j])
+            {
+                /* Set the corresponding flag using the bitmask value */
+                flags |= FLAGS_ARR[j];
+                break;
+            }
+        }
 
-	*i = curr_i - 1;
+        /* Stop processing if the character isn't a valid flag */
+        if (FLAGS_CH[j] == '\0')
+        {
+            break;
+        }
+    }
 
-	return (flags);
+    /* Update the current index pointer */
+    *i = curr_i - 1;
+
+    return flags;
 }
